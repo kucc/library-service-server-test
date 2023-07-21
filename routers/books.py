ea9a38ef51787d21993589859c3424868c2fe8fb
smreosms13 <smreosms13@gamil.com>
2023-07-21 22:09:27 +0900

    Feat : Use get_db in router API functions and include env file for settings

--- routers/books.py
+++ routers/books.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter
-from database import EngineConn
+from fastapi import APIRouter, Depends
+from database import get_db
 from models import Book, BookInfo, BookReview, BookRequest
+from sqlalchemy.orm import Session
 
-engine = EngineConn()
-session = engine.get_session()
 router = APIRouter(prefix="/books", tags=["books"],responses={201 : {"description" : "Success"}, 400 : {"description" : "Fail"}})
 
 # /books 경로에 대한 핸들러 함수
 @router.get("/")
-def get_books():
-    books_info = session.query(Book).all()
+def get_books(
+        db: Session = Depends(get_db)
+):
+    books_info = db.query(Book).all()
     if books_info:
         return books_info
--- routers/users.py
+++ routers/users.py
@@ -1,22 +1,26 @@
 # for /users
-from fastapi import APIRouter
-from database import EngineConn
+from fastapi import APIRouter, Depends
+from database import get_db
+from sqlalchemy.orm import Session
 from models import User
 
-engine = EngineConn()
-session = engine.get_session()
 router = APIRouter(prefix="/users", tags=["users"],responses={201 : {"description" : "Success"}, 400 : {"description" : "Fail"}})
 
 # /users 경로에 대한 핸들러 함수
 # /users/
 @router.get("/")
-def get_users():
-    users_info = session.query(User).all()
+def get_users(
+        db: Session = Depends(get_db)
+):
+    users_info = db.query(User).all()
     if users_info:
         return users_info
 
 # /users/{user_id}
 @router.get("/{user_id}")
-def get_user(user_id: int):
-    user_info = session.query(User).filter(User.user_id == user_id).first()
+def get_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    user_info = db.query(User).filter(User.user_id == user_id).first()
     if user_info: return user_info
