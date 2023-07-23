2a405b83532a32f203e75ed36bb76dff25d19b35
mjkweon17 <mjkweon17@korea.ac.kr>
2023-07-23 19:39:39 +0900

    Test: deploy 1940

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv() # take environment variables from .env.
 DB_DB = os.getenv("DB_DB")
 DB_HOST = os.getenv("DB_HOST")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = int(os.getenv("DB_PORT"))
 DB_USER = os.getenv("DB_USER")
 
 # firebase authentication & authorization
