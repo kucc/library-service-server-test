5303bace5042fbc6696313e59a577fa401a7ea0f
smreosms13 <smreosms13@gamil.com>
2023-07-31 22:07:00 +0900

    Fix : 1. Add validator for date queries in PeriodQuery class 2. Make object to deal with invalid date format 3. Simplize logic for filtering by period query

--- internal/key_validation.py
+++ internal/key_validation.py
@@ -7,3 +7,7 @@ class ForeignKeyValidationError(HTTPException):
 class ItemKeyValidationError(HTTPException):
     def __init__(self, detail:tuple):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid ItemKey, The {detail[0]} {detail[-1]} does not exist.")
+
+class InvalidDateFormatError(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid date format. Dates should be in the format 'YYYY-MM-DD'.")
