c25eaa9c1fdbde0346c52198eaa901797264644a
mjkweon17 <mjkweon17@korea.ac.kr>
2023-08-05 09:13:31 +0900

    Feat: login implementation completed

--- internal/auth.py
+++ internal/auth.py
@@ -1,19 +1,19 @@
 ### internal/auth.py
 import datetime
-from typing import Annotated
+from typing import Annotated, Union
 
-from fastapi import APIRouter, Depends, HTTPException,status, Response
-from fastapi.security import OAuth2PasswordBearer
-from database import get_db
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from pydantic import BaseModel
 
+from database import get_db
 from models import User, Admin, Book
 from config import Settings
+
 from internal.salt import *
 from internal.auth_dependency_schema import *
-
 from internal.custom_exception import ItemKeyValidationError, ForeignKeyValidationError
 from internal.schema import *
 from internal.crudf import *
@@ -22,41 +22,39 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 setting = Settings()
 
-salt_separator = setting.fb_salt_separator
-signer_key = setting.fb_signer_key
-rounds = setting.fb_rounds
-mem_cost = setting.fb_mem_cost
+fb_salt_separator = setting.fb_salt_separator
+fb_signer_key = setting.fb_signer_key
+fb_rounds = setting.fb_rounds
+fb_mem_cost = setting.fb_mem_cost
 
 # /auth 경로에 대한 핸들러 함수
 @router.get("/")
 async def get_auths(
         db: Session = Depends(get_db)
 ):
-    auths = db.query(UserIn).all()
+    auths = db.query(User).all()
     if auths:
         return auths
     else:
         raise HTTPException(status_code=404, detail="No auths found")
 
 # 로그인
-@router.post("/login",
+@router.post("/token",
              status_code=status.HTTP_201_CREATED,
              response_model=Token,
              response_description="Success to login"
              )
-async def login(req: UserIn, db: Session = Depends(get_db)):
-    """Processes user's authentication and returns a token
-    on successful authentication.
-
-    request body:
-
-    - username: Unique identifier for a user e.g email, 
-                phone number, name
-
-    - password:
-    """
-
-    return "token"
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.email == form_data.username).first()
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorret username or password")    
+    if not verify_password(form_data.password, user.password, user.salt, fb_salt_separator, fb_signer_key, fb_rounds, fb_mem_cost):
+        raise HTTPException(status_code=400, detail="Incorret username or password")    
+    return {"access_token": user.email, "token_type": "bearer"}
+    #return {"access_token": create_access_token(data={"sub": user.email}), "token_type": "bearer"}
 
 # 회원가입
 @router.post("/register")
--- internal/auth_dependency_schema.py
+++ internal/auth_dependency_schema.py
@@ -1,55 +1,72 @@
+from datetime import datetime, timedelta
 from typing import Annotated, Union, Any
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from pydantic import BaseModel, validator
-# from typing import List
-# from internal.custom_exception import InvalidDateFormatError
-import os
-from datetime import datetime, timedelta
-from jose import jwt
-from database import get_db
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from jose import jwt
+
+from database import get_db
+
 from internal import firebasescrypt
 
-
-oauth2scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 ########## schema ##########
 
 # Auth - 로그인 정보
-
 class UserIn(BaseModel):
+    user_id: int
+    user_name: str
+    status: bool
     email: str
+    valid: bool
 
+# Password가 맞는지 검사하기 위해 데이터를 이 곳에 먼저 넣음
 class UserInDB(UserIn):
-    password: str
+    salt: str
+    hashed_password: str
 
 ### Auth - 토큰 정보
 class Token(BaseModel):
     access_token: str
     token_type: str
-    email: str
-
-class User(BaseModel):
-    username: str   # email과 동일?
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
 
 ########## dependency ##########
 
-# https://essenceofinvesting.tistory.com/114
-
 # 비밀번호 해싱
-def get_hashed_password(password: str) -> str:
-    salt = "salt"
-    hashed_password = salt + password
-    return hashed_password
+def get_hashed_password():
+    pass
 
 # 비밀번호 검증
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return firebasescrypt.verify(plain_password, hashed_password)
+def verify_password(
+        plain_password: str, 
+        password_hash: str, 
+        salt: str,
+        salt_separator: str,
+        signer_key: str,
+        rounds: int,
+        mem_cost: int
+        ) -> bool:
+    is_valid = firebasescrypt.verify_password(
+        password=plain_password,
+        known_hash=password_hash,
+        salt=salt,
+        salt_separator=salt_separator,
+        signer_key=signer_key,
+        rounds=rounds,
+        mem_cost=mem_cost
+    )
+    return is_valid
+
+
+
+
+
+
+
+
+"""
 
 # 사용법: https://essenceofinvesting.tistory.com/114
 # 공식 문서: https://pypi.org/project/python-jose/
@@ -88,6 +105,9 @@ def get_user(email: str, db: Session):
     if email in db.query(User).filter(User.email == email).first():
         return db.query(User).filter(User.email == email).first()
 
+"""
+
+"""
 # 토큰 검증
 async def get_current_user(token: str = Depends(oauth2scheme)):
     credentials_exception = HTTPException(
@@ -99,3 +119,5 @@ async def get_current_user(token: str = Depends(oauth2scheme)):
     if user is None:
         raise credentials_exception
     return user
+
+"""
