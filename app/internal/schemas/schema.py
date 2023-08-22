9b20f908155c9c97b14a01e34b510228ed2cc751
mjkweon17 <mjkweon17@korea.ac.kr>
2023-08-22 14:32:06 +0900

    Refactor: changed some parts before merging

--- app/internal/schemas/schema.py
+++ app/internal/schemas/schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, validator
 from typing import List
-from app.internal.custom_exception import InvalidDateFormatError
+from internal.custom_exception import InvalidDateFormatError
 import datetime
 
 # TODO :
