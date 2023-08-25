99df224a6fd3336776154306125b0a71e16ee86d
mjkweon17 <mjkweon17@korea.ac.kr>
2023-08-26 04:34:09 +0900

    feat: add user role-based data retrieval based on email
    Implemented a test code in auth.py to retrieve appropriate information based on the user's role (admin or normal user) using their email. Also, added corresponding schema definitions in auth_schema.py.
    
    - auth.py: Added a test function to fetch user information based on their email and role.
    - auth_schema.py: Defined schemas for the data structures related to the role-based retrieval.

--- app/internal/schemas/auth_schema.py
+++ app/internal/schemas/auth_schema.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel, validator
-# from typing import List
-from internal.custom_exception import InvalidDateFormatError
-# import datetime
+from typing import Union
 
+# 일반 사용자와 관리자 공통의 필드가 정의된 스키마
+class AuthBaseUser(BaseModel):
+    user_id: int
+    email: str | None = None
+    user_name: str | None = None
+    status: bool | None = None
+    valid: bool | None = None
+    # access_token: str
+
+# 일반 사용자 스키마
+class AuthUser(AuthBaseUser):
+    pass
+
+# 관리자 스키마
+class AuthAdmin(AuthBaseUser):
+    admin_id: int
+    admin_status: bool
+
+# 일반 사용자와 관리자 스키마를 합친 스키마
+class AuthUserResponse(BaseModel):
+    user: Union[AuthUser, AuthAdmin]
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,10 +1,55 @@
 # /auth 경로에 대한 핸들러 함수
+from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from models import User
+from config import FB_Settings
+
+# from internal.schemas.auth_dependency_schema import *
+# from internal.crudf import *
+
+from internal.schemas.auth_schema import *
+from internal.auth_dependency import *
+
+from sqlalchemy.orm import Session
+from database import get_db
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+setting = FB_Settings()
+
+# 테스트
+# 일반 사용자: mjkweon17@korea.ac.kr
+# 관리자: thisisfortest@gmail.com
+@router.get("/{email}", response_model=AuthUserResponse, status_code=status.HTTP_200_OK, response_description="Get user info")
+async def get_user(email: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == email).first()
+    
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    user_info = {
+        "user_id": user.user_id,
+        "email": user.email,
+        "user_name": user.user_name,
+        "status": user.status,
+        "valid": user.valid
+    }
+
+    if user.admin:
+        user_info["admin_id"] = user.admin.admin_id
+        user_info["admin_status"] = user.admin.admin_status
+        auth_response = AuthUserResponse(user=AuthAdmin(**user_info))
+    else:
+        auth_response = AuthUserResponse(user=AuthUser(**user_info))
+
+    print(auth_response)
+    return auth_response
 
 # 로그인
     # authenticate user
     # create access token
     # return token
-
 
 # 회원가입
 
